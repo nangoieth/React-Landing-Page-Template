88b48da87d759a9f4c05009e1fcf8e12c7c50c32
nangoieth <125410883+nangoieth@users.noreply.github.com>
2023-09-06 15:46:21 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
+import { Faq } from "./components/faq";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
@@ -33,6 +34,7 @@ const App = () => {
       <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
+      <Faq data={landingPageData.Faq} />
       <Contact data={landingPageData.Contact} />
     </div>
   );

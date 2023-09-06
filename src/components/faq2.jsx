a146585d2a6b7c7f39d7b89cced8f6fd66512388
nangoieth <125410883+nangoieth@users.noreply.github.com>
2023-09-06 15:39:39 +0700

    Update faq2.jsx

--- src/components/faq2.jsx
+++ src/components/faq2.jsx
@@ -9,6 +9,9 @@ import {
 
 export default function StaticFaqSection() {
   return (
+      <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
     <MDBContainer>
       <section>
         <MDBTypography
